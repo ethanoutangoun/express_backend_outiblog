e7b338c689610cf5925904c328811bb0dd1f750c
ethanoutangoun <ethanoutangoun@gmail.com>
2023-09-02 02:06:38 -0700

    chnage connecrion URL

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://ethanoutangoun:j5E92M40BnnuImQb@testcluster.mtuva8j.mongodb.net/outiblog', {
+mongoose.connect('mongodb+srv://ethanoutangoun:mlETuDR4i8wlugvR@testcluster.mtuva8j.mongodb.net/outiblog', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
