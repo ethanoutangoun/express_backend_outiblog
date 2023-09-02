b8b1e1331c2d20a5ccdb330ac427102a8315b045
ethanoutangoun <ethanoutangoun@gmail.com>
2023-09-02 02:09:15 -0700

    w

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://ethanoutangoun:mlETuDR4i8wlugvR@testcluster.mtuva8j.mongodb.net/outiblog', {
